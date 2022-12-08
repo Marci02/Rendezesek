9e0cd5d18fa0a328c401aef5f3316c678269c45e
Marci02 <nagymarci.0218@gmail.com>
2022-12-08 13:14:02 +0100

    12_08 órai

---
+++ Beszúró rendezés/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beszúró_rendezés
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beszuró rendezés");
+
+            int[] tomb = new int[] { 5, 3, 10, 6, 1, 2, 4, 8 };
+
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                while (j >= 0 && tomb[j] > tomb[j + 1])
+                {
+                    if (tomb[j] > tomb[j + 1])
+                    {
+                        int temp = tomb[j];
+                        tomb[j] = tomb[j + 1];
+                        tomb[j + 1] = temp;
+                        j--;
+                    }
+                }
+
+            }
+
+            foreach (var t in tomb)
+            {
+                Console.Write($" {t}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JavitottBeszuro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavitottBeszuro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Javított beszuró rendezés");
+
+            int[] tomb = new int[] { 5, 3, 10, 6, 1, 2, 4, 8 };
+
+            for (int i = 1; i < tomb.Length; i++)
+            {
+                int j = i - 1;
+                int temp = tomb[i];
+                while (j >= 0 && tomb[j] > temp)
+                {
+                    tomb[j + 1] = tomb[j];
+                    j--;
+                }
+                tomb[j + 1] = temp;
+            }
+
+
+
+
+            foreach (var t in tomb)
+            {
+                Console.Write($" {t}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
