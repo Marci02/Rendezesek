141bb2c116e8469f32d2e7c536c2a4b07c6148b2
Marci02 <nagymarci.0218@gmail.com>
2022-12-12 08:38:53 +0100

    igen

---
+++ MaximumKivalasztasps/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumKivalasztasps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximumkiváasztásos rendezés");
+
+            int[] tomb = new int[] { 5, 3, 10, 6, 1, 2, 4, 8, 1 };
+
+            for (int i = 6; i > tomb.Length; i--)
+            {
+                int max = 0;
+                for (int j = 1; j < i; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
